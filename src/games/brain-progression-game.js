b61aedffe3b559abc850c217770fb2310e2bb8a9
Vlad Medvedev <nightshiftmaster@gmail.com>
2020-04-20 16:20:24 +0300

    game update23

--- src/games/brain-progression-game.js
+++ src/games/brain-progression-game.js
@@ -4,27 +4,31 @@ import numberGenerator from './number-generator.js';
 
 const gameAlert = ('What number is missing in the progression?');
 
-let numberOfprogress = 0;
-let array2 = [];
-
-const progression = () => {
-  let string = '';
-  let string2 = '';
-  let array = [];
-  const num = (numberGenerator());
-  const step = (Math.floor(Math.random() * 4) + 1);
-  const border = num + (step * 10);
-  for (let i = num; i < border; i += step) {
-    string += (`${i} `);
-    array = string.split(' ');
-    numberOfprogress = (Math.floor(Math.random() * 10));
-    array2 = array[numberOfprogress];
-    array[numberOfprogress] = '..';
-    string2 = array.join(' ');
+let step = 0;
+let progression = [];
+let hiddenNumberIndex;
+const generateProgression = () => {
+  const start = (numberGenerator());
+  progression = [];
+  step = (Math.floor(Math.random() * 4) + 1);
+  const border = start + (step * 10);
+  for (let i = start; i < border; i += step) {
+    progression.push(i);
   }
-  return string2;
+  hiddenNumberIndex = (Math.floor(Math.random() * 10) + 1);
+  progression[hiddenNumberIndex] = '..';
+  const question = progression.join(' ');
+  return question;
 };
-const rightNumber = () => array2;
 
-const progressionGame = () => game(gameAlert, progression, rightNumber);
+const hiddeNumber = (progres) => {
+  let result = 0;
+  for (let n = 0; n < 10; n += 1) {
+    result = (progres[0] + step * hiddenNumberIndex);
+  }
+  return result.toString();
+};
+
+const showNumber = () => hiddeNumber(progression);
+const progressionGame = () => game(gameAlert, generateProgression, showNumber);
 export default progressionGame;
