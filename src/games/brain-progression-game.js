bbe84823cb224571d34ce1962b8e99fc8bb04449
Vlad Medvedev <nightshiftmaster@gmail.com>
2020-04-20 13:32:55 +0300

    game update22

--- src/games/brain-progression-game.js
+++ src/games/brain-progression-game.js
@@ -5,26 +5,26 @@ import numberGenerator from './number-generator.js';
 const gameAlert = ('What number is missing in the progression?');
 
 let numberOfprogress = 0;
-let arr2 = [];
+let array2 = [];
 
 const progression = () => {
   let string = '';
   let string2 = '';
-  let arr = [];
+  let array = [];
   const num = (numberGenerator());
   const step = (Math.floor(Math.random() * 4) + 1);
   const border = num + (step * 10);
   for (let i = num; i < border; i += step) {
     string += (`${i} `);
-    arr = string.split(' ');
+    array = string.split(' ');
     numberOfprogress = (Math.floor(Math.random() * 10));
-    arr2 = arr[numberOfprogress];
-    arr[numberOfprogress] = '..';
-    string2 = arr.join(' ');
+    array2 = array[numberOfprogress];
+    array[numberOfprogress] = '..';
+    string2 = array.join(' ');
   }
   return string2;
 };
-const rightNumber = () => arr2;
+const rightNumber = () => array2;
 
 const progressionGame = () => game(gameAlert, progression, rightNumber);
 export default progressionGame;
